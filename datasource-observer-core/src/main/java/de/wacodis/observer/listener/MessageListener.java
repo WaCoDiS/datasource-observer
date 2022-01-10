efdedf5bb1975a35302b49633127fd78180fcbbe
Sebastian Drost <s.drost@52north.org>
2022-01-10 15:37:43 +0100

    Update license header

--- datasource-observer-core/src/main/java/de/wacodis/observer/listener/MessageListener.java
+++ datasource-observer-core/src/main/java/de/wacodis/observer/listener/MessageListener.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018-2021 52°North Initiative for Geospatial Open Source
+ * Copyright 2018-2022 52°North Initiative for Geospatial Open Source
  * Software GmbH
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
